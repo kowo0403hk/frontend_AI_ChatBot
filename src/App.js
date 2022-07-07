aaedeeb7b58c269fc90b5c92cf8a5d5b9f8a04c6
Alex Cheung <alexc.sfu@gmail.com>
2022-07-06 21:25:04 -0700

    finished basic set up of front-end by using create-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useCallback } from "react";
+import ReactAudioPlayer from "react-audio-player";
+import newSpeech from "../audio/speech.mp3";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [audio, setAudio] = useState("");
+  // since the axios function becomes a dependancy of the useEffect, we have to wrap it inside of
+  const memorizedCallback = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log("axios outgoing request");
+      return axios({
+        method: "POST",
+        url: "/api/openai",
+        data: JSON.stringify({ input }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          console.log("React render stage", res);
+          setInput("");
+        })
+        .catch((e) => console.log(e.message));
+    },
+    [input]
+  );
+
+  // use useEffect to trigger setAudio source so that the virtual dom can be updated with the latest audio. And since this useEffect will be depending on the call of a function that fires the axios request, we need to wrap that function inside of the useEffect hook and put it as a dependancy of the useEffect.
+  useEffect(() => {
+    setAudio(newSpeech);
+  }, [memorizedCallback]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={memorizedCallback}>
+        <input
+          type="text"
+          name="input"
+          placeholder="Ask me a question"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <input type="submit" value="Get a response" />
+      </form>
+      {/* <audio src={audio} controls></audio> */}
+      <ReactAudioPlayer src={audio} controls />
     </div>
   );
 }
